3fd5e60e19f1f5cb5e40a3c546b1815848b78cc6
Volodymyr Avramuk <vavramuk@gmail.com>
2013-03-20 16:47:50 +0200

    Remove manual management of sys.path in tests (nosetests does it for me)

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '../property_caching')))
-
 import unittest2
 from property_caching.decorators import cached_property, clear_cached_property
 
