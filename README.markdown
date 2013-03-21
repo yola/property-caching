33535a1232f76634479299cd9f9c4d2b416e2e23
Volodymyr Avramuk <vavramuk@gmail.com>
2013-03-21 19:59:39 +0200

    Add new cached_in_class_property decorator, add function to explicitly set property cache, fix issue with calling decorated function even in case of cache.

--- README.markdown
+++ README.markdown
@@ -6,23 +6,38 @@ Written and used by the folks at Yola. Check out our [free website][1] builder t
 
 ## Overview
 
-- cached_property - stores results of decorated functions in decorated object
+- cached_property - stores results of decorated methods in decorated object
 (in _cached_properties attribute)
-- clear_cached_property - deletes cached value
+- cached_in_class_property - stores results of decorated methods in the class of decorated object
+(in _cached_in_class_properties attribute). All instances will share cached value.
+- clear_property_cache - deletes cached value (works for object cached properties only)
+- set_property_cache - explicitly sets property cache (works for object cached properties only)
 
 ## Usage
-    from property_caching.decorators import (cached_property, clear_cached_property)
+    from property_caching.decorators import (cached_property,
+                                             cached_in_class_property,
+                                             clear_property_cache,
+                                             set_property_cache)
 
     class Dummy:
         @cached_property
         def foo(self):
-            return expensive_operation()
+            return service.expensive_operation()
+
+        @cached_in_class_property
+        def service(self):
+            return expensive_service_initialization()
 
     d = Dummy()
     d.foo   # calculates result and stores it in d._cached_properties
     d.foo   # uses cached value
 
-    clear_cached_property(d, 'foo')  # clears cache for property `foo`
+    clear_property_cache(d, 'foo')   # clears cache for property `foo`
+    set_property_cache(d, 'foo', 42) # implicitly set cache for property `foo`
+
+    d2 = Dummy()
+    d2.foo  # re-calculates value of `foo` but uses cached service
+
 
 ## Testing
 
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,8 +1,11 @@
 import unittest2
-from property_caching.decorators import cached_property, clear_cached_property
+from property_caching import (cached_property, cached_in_class_property,
+                              clear_property_cache, set_property_cache)
 
 
 class TestClass(object):
+    counter3 = 0
+
     def __init__(self):
         self.counter = 0
         self.counter2 = 0
@@ -17,13 +20,18 @@ class TestClass(object):
         self.counter2 += 1
         return self.counter2
 
+    @cached_in_class_property
+    def method3(self):
+        self.counter3 += 1
+        return self.counter3
+
 
 class BaseTestCase(unittest2.TestCase):
     def setUp(self):
         self.test_obj = TestClass()
 
 
-class DecoratorTestCase(BaseTestCase):
+class CachedPropertyTestCase(BaseTestCase):
     """A method decorated with @cached_property"""
 
     def test_returns_the_calculated_value(self):
@@ -33,19 +41,45 @@ class DecoratorTestCase(BaseTestCase):
     def test_does_not_recalculate_the_value_on_subsequent_calls(self):
         self.assertEqual(self.test_obj.method, 1)
         self.assertEqual(self.test_obj.method, 1)
+        self.assertEqual(self.test_obj.counter, 1)
 
 
-class ClearCachedPropertyTestCase(BaseTestCase):
-    """clear_cached_property(object, method_name)"""
+class CachedInClassPropertyTestCase(BaseTestCase):
+    """A method decorated with @cached_in_class_property"""
+
+    def test_does_not_recalculate_the_value_on_subsequent_calls(self):
+        self.assertEqual(self.test_obj.method3, 1)
+        self.assertEqual(self.test_obj.method3, 1)
+        self.assertEqual(TestClass.counter3, 1)
+
+    def test_shares_cached_property_among_all_objects_of_the_class(self):
+        obj1 = TestClass()
+        self.assertEqual(obj1.method3, 1)
+        obj2 = TestClass()
+        self.assertEqual(obj2.method3, 1)
+        self.assertEqual(TestClass.counter3, 1)
+
+
+class ClearPropertyCacheTestCase(BaseTestCase):
+    """clear_property_cache(object, property_name)"""
 
     def test_clears_the_calculated_value(self):
         self.assertEqual(self.test_obj.method, 1)
-        clear_cached_property(self.test_obj, 'method')
+        clear_property_cache(self.test_obj, 'method')
         self.assertEqual(self.test_obj.method, 2)
 
     def test_does_not_affect_other_cached_properties_on_the_object(self):
         self.assertEqual(self.test_obj.method, 1)
         self.assertEqual(self.test_obj.method2, 1)
-        clear_cached_property(self.test_obj, 'method')
+        clear_property_cache(self.test_obj, 'method')
         self.assertEqual(self.test_obj.method, 2)
         self.assertEqual(self.test_obj.method2, 1)
+
+
+class SetPropertyCacheTestCase(BaseTestCase):
+    """set_property_cache(object, property_name, property_value)"""
+
+    def test_sets_cache_for_the_given_property_name(self):
+        self.assertEqual(self.test_obj.method, 1)
+        set_property_cache(self.test_obj, 'method', 10)
+        self.assertEqual(self.test_obj.method, 10)
