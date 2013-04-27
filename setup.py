2716632984f601e468de1d3592d4a34f787ed3b1
Michael Joseph <michaeljoseph+github@gmail.com>
2013-04-27 12:13:19 +0200

    Add author attribution

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='property-caching',
     version='0.0.2',
     description='Property caching',
+    author='Yola',
+    author_email='tech@yola.com',
     url='https://github.com/yola/property-caching',
     packages=['property_caching'],
 )
