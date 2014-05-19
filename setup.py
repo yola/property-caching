de76083632ce8c888bf301d29ab30be1914b955a
William Stewart <william@synthasite.com>
2014-05-19 14:22:56 +0200

    Bump version and update changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='property-caching',
-    version='1.0.1',
+    version='1.0.2',
     description='Property caching',
     author='Yola',
     author_email='engineers@yola.com',
