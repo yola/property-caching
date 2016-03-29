e8fc519f702b741b9724b285034ebe006f5e9069
Justin Blake <justin@hentzia.com>
2016-03-29 13:51:00 -0400

    Refactor truthy check to be easier to understand

--- property_caching/__init__.py
+++ property_caching/__init__.py
@@ -20,7 +20,7 @@ def cached_property(fn):
 def truthy_cached_property(fn):
     def _truthy_cached_property(self):
         return _get_property_value(
-            fn, self, _OBJ_CACHE_ATTR_NAME, only_cache_true_vals=True)
+            fn, self, _OBJ_CACHE_ATTR_NAME, cache_false_results=False)
     return property(update_wrapper(_truthy_cached_property, fn))
 
 
@@ -45,14 +45,14 @@ def _get_cache(obj, cache_attr_name=_OBJ_CACHE_ATTR_NAME):
     return getattr(obj, cache_attr_name, {})
 
 
-def _get_property_value(fn, obj, cache_attr_name, only_cache_true_vals=False):
+def _get_property_value(fn, obj, cache_attr_name, cache_false_results=True):
     result = None
     cache = _get_cache(obj, cache_attr_name)
     if fn.__name__ not in cache:
         result = fn(obj)
         # re-read cache since it might be already populated by nested functions
         cache = _get_cache(obj, cache_attr_name)
-        if result or not only_cache_true_vals:
+        if result or cache_false_results:
             cache[fn.__name__] = result
         setattr(obj, cache_attr_name, cache)
     return cache.get(fn.__name__, result)
